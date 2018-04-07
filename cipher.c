d596d8bb44ca426e8b228384bed909fb85136ccf
Coop <mjmohr1@shockers.wichita.edu>
2018-04-07 10:12:01 -0500

    minor text changes

--- cipher.c
+++ cipher.c
@@ -1,6 +1,6 @@
 #include "cipher.h"
 
-void encrypt(char *buffer, int seed) {
+void encrypt(char *buffer, int seed) { 
     int work = 0;
     int cur = 0;
     int num = 0;
@@ -12,21 +12,19 @@ void encrypt(char *buffer, int seed) {
 
     FILE *fp;
     fp = fopen(name,"r");
-    if (fp==NULL) { printf("File %s not found!\n",name); exit(1); }
+    if (fp==NULL) { printf("\nFile %s not found!\n",name); return; }
     
     char *temp;    
     temp = (char*) malloc (strlen(buffer));
     temp = strtok(buffer, ".");
 
     result =  sprintf(name, "textfiles/c_%s.txt", temp);
-    if (result==0) { printf("File output naming failed.\n"); exit(1); }
+    if (result==0) { printf("\nFile output naming failed.\n"); return; }
 
     FILE *out;
     out = fopen(name,"w");
 
     srand(seed);
-
-    memset(buffer, 0, strlen(buffer));
 
     while (cur != EOF) {
         result = 0;
@@ -39,15 +37,15 @@ void encrypt(char *buffer, int seed) {
         }
 
         work = cur ^ num;
-        result = fputc(work, out);
-        if (result==EOF) { printf("Printing to file failed.\n"); exit(1); }
+        result = putc(work, out);
+        if (result==EOF) { printf("\nPrinting to file failed.\n"); return; }
     }; 
 
     fclose(fp);
     fclose(out);
 }
 
-void decrypt(char *buffer, int seed) {
+void decrypt(char *buffer, int seed) { 
     int work = 0;
     int cur = 0;
     int num = 0;
@@ -59,21 +57,19 @@ void decrypt(char *buffer, int seed) {
 
     FILE *fp;
     fp = fopen(name,"r");
-    if (fp==NULL) { printf("File %s not found!\n",name); exit(1); }
+    if (fp==NULL) { printf("\nFile %s not found!\n",name); return; }
     
     char *temp;    
     temp = (char*) malloc (strlen(buffer));
     temp = strtok(buffer, ".");
 
     result =  sprintf(name, "textfiles/p_%s.txt", temp);
-    if (result==0) { printf("File output naming failed.\n"); exit(1); }
+    if (result==0) { printf("\nFile output naming failed.\n"); return; }
 
     FILE *out;
     out = fopen(name,"w");
 
     srand(seed);
-
-    memset(buffer, 0, strlen(buffer));
 
     while (cur != EOF) {
         result = 0;
@@ -85,11 +81,9 @@ void decrypt(char *buffer, int seed) {
             num = abs(rand() % 256);
         }
 
-        work = cur ^ num;
-        result = fputc(work, out);
-        if (result==EOF) { printf("Printing to file failed.\n"); exit(1); }
+        result = putc(work, out);
+        if (result==EOF) { printf("\nPrinting to file failed.\n"); return; }
     }; 
-
     fclose(fp);
     fclose(out);  
 }
